f418615e628fc6a4a273aadf58b1fb7e6b346ba2
AK3 <mjlabishek123@gmail.com>
2026-05-02 22:14:05 +0545

    almost made a complete enemy

--- scenes/Player/enemy.gd
+++ scenes/Player/enemy.gd
@@ -1,1 +1,19 @@
 extends CharacterBody2D
+
+@export_category("Stats")
+@export var hitpoints: int = 180
+@export_category("Realted Scenes")
+@export var death_packed: PackedScene
+
+
+func take_damage(damage_taken: int) -> void:
+	hitpoints -= damage_taken
+	if hitpoints <= 0:
+		death()
+		
+		
+func death() -> void:
+	var death_scene: Node2D = death_packed.instantiate()
+	death_scene .position = global_position + Vector2(0.0, -32.0)
+	%Effects.add_child(death_scene)
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,7 @@ enum State {
 @export_category("Stats")
 @export var speed: int = 400
 @export var attack_speed: float = 0.6 
+@export var attack_damage: int = 60
 
 var state: State = State.IDLE
 var move_direction: Vector2 =  Vector2(0,0)
@@ -73,3 +74,7 @@ func attack() -> void:
 	
 	await get_tree().create_timer(attack_speed).timeout
 	state = State.IDLE
+
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	area.owner.take_damage(attack_damage)
