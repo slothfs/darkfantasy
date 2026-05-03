8bb8cf61fea44403deffe19b29f72c4e41f129fb
AK3 <mjlabishek123@gmail.com>
2026-05-03 17:28:36 +0545

    made level ui now scripting it

---
+++ scripts/level_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+func _on_a_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_a_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_a_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_a_4_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_a_5_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_a_6_pressed() -> void:
+	pass # Replace with function body.
