ee15730484651c40469256347cace9655acf5605
AK3 <mjlabishek123@gmail.com>
2026-05-03 14:00:34 +0545

    made level 4 and 5

---
+++ scenes/levels/door3.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+@export var alert_scene: PackedScene = preload("res://scenes/door_alert.tscn")
+var current_alert: Node = null
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player(scene)") or body.name == "Player":
+		if body.get("has_key") == true:
+			call_deferred("_transition_level")
+		else:
+			_show_alert()
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player(scene)") or body.name == "Player":
+		_hide_alert()
+
+func _show_alert() -> void:
+	if not current_alert and alert_scene:
+		current_alert = alert_scene.instantiate()
+		if get_tree().current_scene:
+			get_tree().current_scene.add_child(current_alert)
+
+func _hide_alert() -> void:
+	if current_alert:
+		current_alert.queue_free()
+		current_alert = null
+
+func _transition_level() -> void:
+	var next_level = "res://scenes/levels/level_4.tscn"
+	if ResourceLoader.exists(next_level):
+		get_tree().change_scene_to_file(next_level)
+	else:
+		print("Next level not found, restarting current level.")
+		get_tree().reload_current_scene()
